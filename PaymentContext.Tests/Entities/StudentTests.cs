7327fcd02cf8c16e743ec2dfd2953c325b99bd25
Kauan <kauanhindlmayer07@gmail.com>
2022-11-15 10:42:09 -0300

    feat: add Notifications

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -11,9 +11,7 @@ namespace PaymentContext.Tests
     [TestMethod]
     public void AdicionarAssinatura()
     {
-      // var subscription = new Subscription(null);
-      // var student = new Student(new Name("Kauan", "Hindlmayer"), new Document("000.000.000-00", EDocumentType.CPF), new Email("kauan.h@gmail.com"));
-      // student.AddSubscription(subscription);
+      
     }
   }
 }
