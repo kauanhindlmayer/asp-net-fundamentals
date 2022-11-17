876c31b103229828eba217d598f12c35ffe2f368
Kauan <kauanhindlmayer07@gmail.com>
2022-11-17 15:38:37 -0300

    test: add CreateBoletoSubscriptionCommandTests

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using Flunt.Validations;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests
+{
+  [TestClass]
+  public class CreateBoletoSubscriptionCommandTests
+  {
+    // Red, Green, Refactor
+    [TestMethod]
+    public void ShouldReturnErrorWhenNameIsInvalid ()
+    {
+      var command = new CreateBoletoSubscriptionCommand();
+      command.FirstName = "";
+      command.Validate();
+      // Assert.AreEqual(false, command.Valid);
+    }
+  }
+}
