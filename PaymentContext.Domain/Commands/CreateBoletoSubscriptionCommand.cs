db6355f0ed01a1850f4f3fca7d3b43a5221a7c26
Kauan <kauanhindlmayer07@gmail.com>
2022-11-17 14:37:13 -0300

    feat: add Commands

---
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -0,0 +1,31 @@
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Domain.Commands
+{
+  public class CreateBoletoSubscriptionCommand
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public string Email { get; set; }
+
+    public string BarCode { get; set; }
+    public string BoletoNumber { get; set; }
+    public string PaymentNumber { get; set; }
+    public DateTime PaidDate { get; set; }
+    public DateTime ExpireDate { get; set; }
+    public decimal Total { get; set; }
+    public decimal TotalPaid { get; set; }
+    public string Payer { get; set; }
+    public EDocumentType PayerDocumentType { get; set; }
+
+    public string Street { get; private set; }
+    public string Number { get; private set; }
+    public string Neighborhood { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; }
+    public string ZipCode { get; private set; }
+  }
+}
